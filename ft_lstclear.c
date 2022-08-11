25c1dc24e2931a326b6f2c1c4ab7f2373c4e2755
iryoga <iryoga@student.42tokyo.jp>
2022-08-12 05:43:18 +0900

    lstclear NULL check

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: iryoga <iryoga@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 04:36:44 by iryoga            #+#    #+#             */
-/*   Updated: 2022/08/12 04:43:35 by iryoga           ###   ########.fr       */
+/*   Updated: 2022/08/12 05:42:31 by iryoga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*tmp;
 
-	if (lst == NULL)
+	if (lst == NULL || del == NULL)
 		return ;
 	while (*lst != NULL)
 	{
